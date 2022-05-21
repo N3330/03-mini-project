e132271ae4b92e89594647147f128064235b94e6
James Giraldo <stackz@protonmail.com>
2022-05-21 12:30:41 -0400

    added special characters array and changed variables to arrays

--- script.js
+++ script.js
@@ -2,29 +2,62 @@
 var generateBtn = document.querySelector("#generate");
 
 //variables holding each character type
-
+var passLength = 8;
+var specialCharacters = ['!','@','#','$','%','^','&','*','(',')','[',']','{','}','"','|','?','/','<','>'];
 var numbers = "0123456789";
 var capLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-// var capLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var lowerLetters = capLetters.toLowerCase();
+var passCriteria = [];
 console.log(numbers);
 console.log(capLetters.length);
 console.log(lowerLetters);
 
-//generate password function
 
-function generatePassword() {
-  console.log('Clicked Button')
-}
+//  prompt if lowercase,uppercase, special characters
+// 2. validate input that 8-128 and at least one character type is being selected
+// 3. generate random password based on selections
+
+// 4. display the password
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
-
+  
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+
+
+
+//generate password function
+
+function generatePassword() {
+  console.log('Clicked Button')
+
+
+  return "Generated Password goes here"
+  }
+
+
+
+// Function for password lenght prompt ParseInt to convert it to interger from string
+
+
+function userPrompt() {
+  passLength = parseInt(prompt('Length of password? 8-128 characters'));
+
+//Checking if its not a number
+if(isNaN(passLength) || passLength < 8 || passLength >128) {
+  alert("Password length has to be number between 8-128");
+  return false;
+}
+
+}
