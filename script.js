bbcad277fa16d4551215126e4def865460141e53
James Giraldo <stackz@protonmail.com>
2022-05-21 12:49:48 -0400

    wrapped confirms with if to ask criteria

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ var generateBtn = document.querySelector("#generate");
 //variables holding each character type
 var passLength = 8;
 var specialCharacters = ['!','@','#','$','%','^','&','*','(',')','[',']','{','}','"','|','?','/','<','>'];
-var numbers = "0123456789";
-var capLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lowerLetters = capLetters.toLowerCase();
+var numbers = ['0','1','2','3','4','5','6','7','8','9'];
+var capLetters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+//use arrow function to utilize toLowerCase() and convert capLetters array into lower case
+var lowerLetters = capLetters.map(lowercase => lowercase.toLowerCase());
 var passCriteria = [];
 console.log(numbers);
 console.log(capLetters.length);
@@ -37,7 +38,7 @@ function writePassword() {
 
 
 
-//generate password function
+//generate password function based on prompt criteria
 
 function generatePassword() {
   console.log('Clicked Button')
@@ -52,12 +53,30 @@ function generatePassword() {
 
 
 function userPrompt() {
-  passLength = parseInt(prompt('Length of password? 8-128 characters'));
+  passLength = parseInt(prompt('What length would you like your password? 8-128 characters'));
 
 //Checking if its not a number
-if(isNaN(passLength) || passLength < 8 || passLength >128) {
-  alert("Password length has to be number between 8-128");
+if(isNaN(passLength) || passLength < 8 || passLength > 128) {
+  alert("Password length has to be number 8-128 characters");
   return false;
 }
+//popup asking if they want lowercase letters
+if(confirm("Want lowercase letters in your passwork?")) {
+  passCriteria = passCriteria.concat(lowerLetters);
+}
+//popup asking if they want uppercase letters
+if(confirm("Want uppercase letters in your passwork?")) {
+  passCriteria = passCriteria.concat(capLetters);
+}
+//popup asking if they want special characters
+if(confirm("Want special characters in your passwork?")) {
+  passCriteria = passCriteria.concat(specialCharacters);
+}
+//popup asking if they want numbers
+if(confirm("Want numbers in your passwork?")) {
+  passCriteria = passCriteria.concat(lowerLetters);
+}
+
+return true;
 
 }
