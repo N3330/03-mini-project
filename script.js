d6613b7e0d9e873651b80a84d439d3f525759b5b
James Giraldo <stackz@protonmail.com>
2022-05-22 16:08:16 -0400

    applied tutoring suggestions userPrompt now returns an object with criteria and length

--- script.js
+++ script.js
@@ -7,9 +7,11 @@ var capLetters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P
 //use arrow function to utilize toLowerCase() and convert capLetters array into lower case
 var lowerLetters = capLetters.map(lowercase => lowercase.toLowerCase());
 var passCriteria = [];
+var passwordArray = [];
 console.log(numbers);
 console.log(capLetters.length);
 console.log(lowerLetters);
+console.log(passCriteria)
 
 
 //  prompt if lowercase,uppercase, special characters
@@ -27,47 +29,38 @@ generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
-  var goodCriteria = userPrompt(); // should be true based on userPrompt functions on line 67
+  var password = generatePassword(); 
   var passwordText = document.querySelector("#password");
   
-  
-  if(goodCriteria) {
-    var generatedPassword = generatePassword();
-    
-    passwordText.value = generatedPassword;
-  } else {
-    passwordText.value = ""; // leaves password blank if password is not a good criteria
-  }
+
+passwordText.value = password;
 
 
 }
 
 
-
-
-
 //generate password function based on prompt criteria
 
 function generatePassword() {
-  var password = "";
-  for(var i = 0; i < passLength; i++) { //loops goes for how long password length user selected 
-    var createRandom = Math.floor(Math.random() * passCriteria.length);// used mathfloor to round mathrandom decimal multipled by lenght of 
-    password = password + passCriteria[createRandom]; // set password to password + the password criteria which is blank with a random index 
+  var options = userPrompt(); //create options 
+  for(var i = 0; i < options.passLength; i++) { //loops goes for how long password length user selected 
+// used mathfloor to round mathrandom decimal and used the options array of total chracters 
+    var createRandom = Math.floor(Math.random() * options.passCriteria.length); 
+    console.log(createRandom);
+    var digit = options.passCriteria[createRandom]; // set password to password + the password criteria which is blank with a random numbner 
+    passwordArray.push(digit); //pushed the data into the array 
 
   }
-  return password;
+  return passwordArray.join("");
 
   }
 
 
 
 // Function for password length prompt ParseInt to convert it to interger from string
-
-
 function userPrompt() {
-  passCriteria = [];
   passLength = parseInt(prompt('What length would you like your password? 8-128 characters'));
-
+  console.log(passLength);
 //Checking if its not a number
 if(isNaN(passLength) || passLength < 8 || passLength > 128) {
   alert("Password length has to be number 8-128 characters");
@@ -87,9 +80,9 @@ if(confirm("Want special characters in your password?")) {
 }
 //popup asking if they want numbers
 if(confirm("Want numbers in your password?")) {
-  passCriteria = passCriteria.concat(lowerLetters);
+  passCriteria = passCriteria.concat(numbers);
 }
-
-return true;
+ console.log(passCriteria);
+return {passCriteria,passLength}; //create passCriteria and passLength object to be used  
 
 }
